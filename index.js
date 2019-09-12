4ca96cc5676fad07c02e8754366d23f4e3c36600
Abraham Emmanuel <info@zetamindgroup.com>
2019-09-12 16:34:58 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('App Listening on port 300!');
+  // eslint-disable-next-line prettier/prettier
+});
