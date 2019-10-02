bc95c43b2421fbcb6a3089d013c42e589a90dc8c
Abraham Emmanuel <info@zetamindgroup.com>
2019-10-02 17:14:00 +0100

    test

---
+++ src/test/auth/auth.test.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import request from 'supertest';
+import mongoose from 'mongoose';
+import faker from 'faker';
+import {
+  expect
+} from 'chai';
+import app from '../server';
+import assert from 'assert';
+import User from '../database/models/User';
+
+describe('App basics', () => {
+  beforeEach(done => {
+    //Before each test we empty the database
+    User.deleteMany({}, err => {
+      done();
+    });
+  });
+
+  it('should be able to sign up a new user', done => {
+    const email = 'john@test.com';
+    const name = 'test';
+    let user = {
+      name: name,
+      email: email,
+      password: '123456'
+    };
+    request(app)
+      .post('/api/auth/register/')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.deep.property(
+          'user.email',
+          'email'
+        );
+        done();
+      });
+  });
+
+  it('should not be able to sign in user with invalid email', done => {
+    const name = 'test';
+    let user = {
+      email: 'test@test.com',
+      password: '123456'
+    };
+    request(app)
+      .post('/api/auth/login/')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+        done();
+      });
+  });
+
+  after(function (done) {
+    return mongoose.disconnect(done);
+  });
+});
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -7,16 +7,8 @@ import app from '../server';
 import assert from 'assert';
 import User from '../database/models/User';
 
-describe('Empty database', () => {
-  beforeEach(done => {
-    //Before each test we empty the database
-    User.remove({}, err => {
-      done();
-    });
-  });
-});
-
 describe('App basics', () => {
+
   it('get index route', done => {
     request(app)
       .get('/')
@@ -29,24 +21,7 @@ describe('App basics', () => {
       });
   });
 
-  // it('should be able to sign up a new user', (done) => {
-  //     const email = faker.internet.email();
-  //     const name = "test"
-  //     let user = {
-  //       name: name,
-  //       email: email,
-  //       password: "123456"
-  //     }
-  //     request(app)
-  //       .post('/api/auth/register/')
-  //       .send(user)
-  //       .set('Accept', 'application/json')
-  //       .expect('Content-Type', /json/)
-  //       .expect(201)
-  //       .end((err, res) => {
-  //         expect(res.body).to.be.an('object');
-  //         done();
-  //       });
-  // });
+  after(function(done) {
+    return mongoose.disconnect(done);
+  });
 });
-
