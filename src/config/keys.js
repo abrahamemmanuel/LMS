34ac1e248da22966a083717e0bc28d5fa776cd25
Abraham Emmanuel <info@zetamindgroup.com>
2019-10-09 11:36:09 +0100

    test(still configuring MONGODB with travis)

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,7 @@
 const key = {
   MONGODB_DEV: 'mongodb://localhost/loanApp',
-  MONGODB_PROD: 'mongodb+srv://emmanuel:mypass123@lms-yultn.mongodb.net/LMS?retryWrites=true&w=majority',
+  MONGODB_PROD:
+    'mongodb+srv://emmanuel:mypass123@lms-yultn.mongodb.net/LMS?retryWrites=true&w=majority',
   MONGODB_TEST: 'mongodb://localhost/lms_test',
   env: process.env.PORT || 5000,
   secret: 'secret'
